662e6585d902d9b1e692ed6896ef5cbb9b19b1f3
Danrus110 <danrus228zk@gmail.com>
2024-12-23 20:46:55 +0300

    make new install script that can install psutil

--- setup.sh
+++ setup.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-chmod +x pyrfetch
-sudo mv $(pwd)/pyrfetch /bin 
+if [ -f /etc/debian_version ]; then
+    sudo apt-get update
+    sudo apt-get install -y python3-psutil
+elif [ -f /etc/redhat-release ]; then
+    sudo yum install -y python3-psutil
+elif [ -f /etc/arch-release ]; then
+    sudo pacman -S --noconfirm python-psutil
+else
+    echo "Unsupported distribution, trying to install via pip"
+    if command -v pip3 &> /dev/null; then
+        pip3 install psutil
+    else
+        echo "pip3 not found, please install manually \"psutil\" library and run install.sh:"
+        echo "pip3 install psutil"
+        echo "sh ./install.sh"
+        exit 1
+    fi
+fi
+
+sh ./install.sh
 
 # Scaryyy asf
-sudo rm -rf ~/pyrfetch
+# sudo rm -rf ~/pyrfetch # This directory could in theory be with other files. i think it's better to use the command below:
+
+sudo rm -rf $(pwd)
